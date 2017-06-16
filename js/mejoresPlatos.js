aa0d50f7b31db8a4be7f0dd14ebe30a13930c8db
Johan Canales <johancanales@MacBook-Pro-de-Johan.local>
2017-06-16 15:58:05 -0500

    Creacion del grafico y agrego trabajos por hacer

--- js/mejoresPlatos.js
+++ js/mejoresPlatos.js
@@ -1,18 +1,107 @@
 console.log("funciono");
 
-var categories = ['','Arroz con pollo','Arroz con carne','lasaña'];
-var dollars = [100,100,100];
-var colors = ['#0000b4','#0082ca','#0094ff'];
+var data = [{
+                "name": "Arooz con Pollo",
+                "value": 20,
+                "image": "../img/logo.png"
+        },
+            {
+                "name": "Lasaña de Carne",
+                "value": 12,
+ 				"image": "../img/logo.png"               
+        },
+            {
+                "name": "Pollo con Champiñones",
+                "value": 19,
 
-var rango = 10;
+        },
+            {
+                "name": "Costillas BBQ",
+                "value": 5,
+				"image": "../img/logo.png"
+        },
+            {
+                "name": "Pure con Carne",
+                "value": 16,
+   				"image": "../img/logo.png"             
+ 
+        }];
 
-var grid = d3.range(rango).map(function(i){
-			return {'x1':0,'y1':0,'x2':0,'y2':480};
-		});
+/* Ordena los datoa */
 
-var tickVals = grid.map(function(d,i){
-			if(i>0){ return i*10; }
-			else if(i===0){ return "100";}
-		});
+data = data.sort(function (a, b) {
+            return d3.ascending(a.value, b.value);
+        })
+var margin = {
+            top: 15,
+            right: 25,
+            bottom: 15,
+            left: 200
+ };
+
+  var width = 960 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+
+ var svg = d3.select("#graphic").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scale.linear()
+            .range([0, width])
+            .domain([0, d3.max(data, function (d) {
+                return d.value;
+            })]);
+
+var y = d3.scale.ordinal()
+            .rangeRoundBands([height, 0], .1)
+            .domain(data.map(function (d) {
+                return d.name;
+            }));
+var yAxis = d3.svg.axis()
+            .scale(y)
+            //no tick marks
+           // .tickSize(0) 
+            .orient("left");
+
+ var gy = svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+ var bars = svg.selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("g")
+
+        //append rects
+bars.append("rect")
+            .attr("class", "bar")
+            .attr("y", function (d) {
+                return y(d.name);
+            })
+            .attr("height", y.rangeBand())
+            .attr("x", 0)
+            .attr("width", function (d) {
+                return x(d.value);
+            });
+
+        //add a value label to the right of each bar
+bars.append("text")
+            .attr("class", "label")
+            //y position of the label is halfway down the bar
+            .attr("y", function (d) {
+                return y(d.name) + y.rangeBand() / 2 + 4;
+            })
+            //x position is 3 pixels to the right of the bar
+            .attr("x", function (d) {
+                return x(d.value) + 3;
+            })
+            .text(function (d) {
+                return d.value;
+            });
+
+
+
 
 
